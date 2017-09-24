af94eec672a67975add5f9a66dd1eec33e885834
Stepan Mracek <stepan.mracek@gmail.com>
2017-09-24 20:36:15 +0200

    initial commit

---
+++ meteo.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import requests
+import json
+import datetime
+
+
+values = {"temperature": None, "humidity": None}
+firebase_url = "https://<db-name>.firebaseio.com
+
+def on_connect(client, userdata, session, rc):
+    client.subscribe("temperature")
+    client.subscribe("humidity")
+
+
+def on_message(client, userdata, msg):
+    value = float(msg.payload.decode("utf-8"))
+    values[msg.topic] = value
+    if (msg.topic == "humidity" and
+        values["temperature"] is not None and
+        values["humidity"] is not None):
+
+        key = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        url = firebase_url + "/meteo/" + key + ".json"
+        response = requests.put(url, data=json.dumps(values))
+        print(response)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("localhost", 1883, 60)
+client.loop_forever()
+
