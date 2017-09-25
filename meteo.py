156e04d703f10ce1c5333033cf27a955fdeccd0d
Stepan Mracek <stepan.mracek@gmail.com>
2017-09-25 23:24:21 +0200

    support for multiple IoT devices

--- meteo.py
+++ meteo.py
@@ -11,26 +11,41 @@ if len(sys.argv) < 2:
     print("usage: meteo.py <firebase_url>")
     exit(1)
 
-values = {"temperature": None, "humidity": None}
+values = {}
+info = {}
 firebase_url = sys.argv[1]
 
 
 def on_connect(client, userdata, session, rc):
-    client.subscribe("temperature")
-    client.subscribe("humidity")
+    client.subscribe("device/+/+")
 
 
 def on_message(client, userdata, msg):
-    value = float(msg.payload.decode("utf-8"))
-    values[msg.topic] = value
-    if (msg.topic == "humidity" and
-        values["temperature"] is not None and
-        values["humidity"] is not None):
+    value = msg.payload.decode("utf-8")
+    topicItems = msg.topic.split('/')
+    device = topicItems[1]
+    topic = topicItems[2]
 
-        key = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        url = firebase_url + "/meteo/" + key + ".json"
-        response = requests.put(url, data=json.dumps(values))
-        print(response)
+    if device not in values:
+        values[device] = {}
+
+    if topic == "info":
+        info[device] = value.split(':')
+        url = firebase_url + "/devices/" + device + ".json"
+        data = dict([(i, True) for i in info[device]])
+        requests.put(url, data=json.dumps(data))
+    else:
+        print(topic, value)
+        values[device][topic] = float(value)
+
+        if (device in info and
+            len(info[device]) > 0 and
+            topic == info[device][0] and
+            len(info[device]) == len(values[device])):
+
+            key = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            url = firebase_url + "/values/" + device + '/' + key + ".json"
+            requests.put(url, data=json.dumps(values[device]))
 
 
 client = mqtt.Client()
